bd88fb244255e83e014dd01675befdb9f11dc442
Axel Frosthagen <axel.frosthagen@edu.orebro.se>
2016-03-18 10:11:46 +0100

    herpherp

--- VäderAPI/VäderAPI/Controllers/HomeController.cs
+++ VäderAPI/VäderAPI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HtmlAgilityPack;
 
 namespace VäderAPI.Controllers
 {
@@ -11,6 +12,16 @@ namespace VäderAPI.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+            HtmlDocument page = new HtmlWeb().Load("http://83.255.197.77/");
+
+            var windSpeed = page.DocumentNode.SelectSingleNode("//p[@id='windSpeed']");
+            string windSpeedValue = windSpeed.InnerText;
+            ViewData["windSpeed"] = windSpeedValue;
+
+            
+
+
 
             return View();
         }
---
+++ VäderAPI/VäderAPI/Controllers/weatherController.cs
@@ -0,0 +1,51 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VäderAPI.Models;
+
+namespace VäderAPI.Controllers
+{
+    public class weatherController : ApiController
+    {
+        // GET: api/weather       
+        weather[] Weather = new weather[] 
+        { 
+
+
+            new weather { Id = 1, windSpeed = "Tomato Soup", windDirection = "Groceries"}
+        };
+
+
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+
+        }
+
+        // GET: api/weather/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/weather
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/weather/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/weather/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
