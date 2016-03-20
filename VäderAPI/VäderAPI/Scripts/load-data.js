f261feccccc15988b73aad4156081713203d4044
Jesper Sundstrom <jesper.sundstrom@hotmail.com>
2016-03-20 21:29:15 +0100

    Updates

---
+++ VäderAPI/VäderAPI/Scripts/load-data.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    // Send an AJAX request
+
+
+    $.ajax({
+        url: 'http://83.255.196.114:25565/api/weather',
+        dataType: 'json',
+        type: 'get',
+        cache: false,
+        success: function (data) {
+            // On success, 'data' contains a list of products.
+            $("#products").empty();
+
+            $.each(data, function (key, item) {
+                // Add a list item for the product.
+                $('<li>', { text: "Vindhastighet: " + item.windSpeed + "m/s" }).appendTo($('#products'));
+                $('<li>', { text: "Vindriktning: " + item.windDirection + "°" }).appendTo($('#products'));
+                $('<li>', { text: "Vindpustar: " + item.gustSpeed + "m/s" }).appendTo($('#products'));
+                $('<li>', { text: "Luft temperatur: " + item.airTemp + "° C" }).appendTo($('#products'));
+                $('<li>', { text: "Vatten temperatur: " + item.waterTemp + "° C" }).appendTo($('#products'));
+                $('<li>', { text: "Batteri: " + item.battery + "%" }).appendTo($('#products'));
+                $('<li>', { text: "Senast uppdaterad: " + item.lastUpdate }).appendTo($('#products'));
+
+            })
+
+
+
+
+
+        },
+
+        error: function (data) {
+            $('<li>', { text: " Datan kunde ej laddas, Försök igen senare eller kontrollera anslutningen" }).appendTo($('#products'));
+
+        }
+
+    });
+});
+
