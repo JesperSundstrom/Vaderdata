383406a3a51ea706abbd5886c7ccb210676fbd97
Axel Frosthagen <axel.frosthagen@edu.orebro.se>
2016-04-14 14:22:02 +0200

    funkar mats är nöjd

--- HTML/js/load-data.js
+++ HTML/js/load-data.js
@@ -45,12 +45,12 @@ $(document).ready(function () {
                 needle.x = 125;
                 needle.regX = 125;
                 needle.regY = 125;
-                needle.rotation =  direction  - 180;
+                needle.rotation =  direction - 179;
                 canvas.addChild(needle);
 
                 createjs.Tween.get(needle, { loop: true })
-                    .to({ rotation: direction - 1 }, 2000, createjs.Ease.getPowInOut(2))
-                    .to({ rotation: direction + 1 }, 2000, createjs.Ease.getPowInOut(2));
+                    .to({ rotation: direction - 181 }, 1000, createjs.Ease.getPowInOut(2))
+                    .to({ rotation: direction - 179 }, 2000, createjs.Ease.getPowInOut(2));
 
                 var circles = new createjs.Bitmap("./img/Bubbles.png");
                 circles.scaleX = .5;
